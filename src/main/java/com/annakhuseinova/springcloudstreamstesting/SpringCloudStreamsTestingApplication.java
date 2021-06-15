a2b8b215868d185524f166cf6dc782dd1549b407
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-15 13:16:51 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springcloudstreamstesting/SpringCloudStreamsTestingApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamstesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsTestingApplication.class, args);
+    }
+
+}
